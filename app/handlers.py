3ce77986b91e6edf3f6342575e42eefde8b55117
shiro <shirochenkov1337@gmail.com>
2026-03-21 17:29:33 +0800

    Refactor project structure

---
+++ app/handlers.py
@@ -0,0 +1,25 @@
+from .legacy import (
+    handle_callback_action,
+    handle_command,
+    handle_max_update,
+    handle_public_start,
+    handle_telegram_update,
+    handle_text_message,
+    open_catalog,
+    safe_answer_callback_query,
+    safe_edit_or_send,
+    start_new_order_flow,
+)
+
+__all__ = [
+    "handle_callback_action",
+    "handle_command",
+    "handle_max_update",
+    "handle_public_start",
+    "handle_telegram_update",
+    "handle_text_message",
+    "open_catalog",
+    "safe_answer_callback_query",
+    "safe_edit_or_send",
+    "start_new_order_flow",
+]
